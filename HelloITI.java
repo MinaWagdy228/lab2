ceb7f97931fb47c5f39b1666b775df4cc93a25b6
Mina Wagdy <minawagdy2228@gmail.com>
2025-12-28 12:26:13 +0200

    Added printHelloGit() method

--- HelloITI.java
+++ HelloITI.java
@@ -2,7 +2,11 @@
 		void printHelloITI(){
 			System.out.println("Hello World!");
 		}
+		void printHelloGit(){
+			System.out.println("Hello Git!");
+		}
 		public static void main(String[] args){
 			printHelloITI();
+			printHelloGit();
 		}
 	}
